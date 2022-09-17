6f5fce0d23e7c025596c95dad53e260720fd1673
Adefolarin Samson Adesoba <a.adesoba@native.semicolon.africa>
2022-09-17 22:55:38 +0100

    Initial commit

--- order_process_app/admin.py
+++ order_process_app/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
-
+from .models import Category, Customers
 # Register your models here.
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = [
+        'category_name', 'description'
+    ]
+    list_filter = [
+        'category_name', 'description'
+    ]
+
+
+@admin.register(Customers)
+class CustomersAdmin(admin.ModelAdmin):
+    list_display = [
+        'company_name', 'contact_name',
+    ]
